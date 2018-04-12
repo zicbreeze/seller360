d12925972e78e0c304949bd39fffa6a6f903d510
zicbreeze <piyawat.pitakpawatkul@lazada.com>
2018-04-12 11:41:30 +0700

    seller360

---
+++ src/main/java/com/lazada/seller360/seller360/Seller360Application.java
@@ -0,0 +1,12 @@
+package com.lazada.seller360.seller360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seller360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seller360Application.class, args);
+	}
+}
